c522a102c3c9375f9fe73281db4af8099b19ea85
jhayashi1 <jaredhayashi@gmail.com>
2024-06-18 14:20:13 -0500

    fix: properly get html from current tab

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from 'react';
+import { useAsyncEffect } from '../helpers/use-async-effect';
 
 export const Main = () => {
     const [tabContent, setTabContent] = useState('');
 
-    useEffect(() => {
-        async function getTabContent() {
-            const content = await chrome.runtime.sendMessage({ action: 'getDOM', });
-            console.log('tab');
-            setTabContent(content);
-        }
-        getTabContent();
+    useAsyncEffect(async () => {
+        const content = await getTabContent()
+        setTabContent(content!);
     }, []);
 
     return (
         <div>{tabContent}</div>
     );
 };
+
+const getTabContent = async () => {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    const id = tab.id
+    const [{result}] = await chrome.scripting.executeScript({
+        target: {tabId: id!},
+        func: () => document.documentElement.innerText,
+    });
+
+    return result
+}
--- src/content.ts
+++ src/content.ts
@@ -1,12 +1,14 @@
 // Function to get the DOM content
-const getDOMContent = (): string => {
+const getDomContent = (): string => {
     return document.documentElement.outerHTML;
 };
-  
+
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener((request, _, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.action === 'getDOM') {
-        const domContent = getDOMContent();
-        sendResponse({ domContent, });
+        let queryOptions = { active: true, lastFocusedWindow: true };
+        // `tab` will either be a `tabs.Tab` instance or `undefined`.
+        let [tab] = await chrome.tabs.query(queryOptions);
+        sendResponse({ tab });
     }
 });
